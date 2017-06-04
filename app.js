e2d90ae1196026f7783d332940fd1550e6c6b27d
Willam Zavala <ricardozv_28@hotmail.com>
2017-06-04 12:43:46 -0600

    adding passport to app.js

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/change_name');
+const passport = require('passport');
+const LocalStrategy - require('passport-local').Strategy;
+
+const app = express();
 
 // Routes
 // const route = require('./routes/route');
+const auth = require('./routes/auth');
 
-const app = express();
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -17,8 +21,28 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, './client/build')));
 
+// Express session
+app.use(require('express-session')({
+  secret: process.env.SESSION_SECRET || 'secret',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    httpOnly: false,
+    secure: false
+  }
+}));
+
+// Setup passport
+app.use(passport.initialize());
+app.use(passport.session());
+const User = require('./models/user');
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 // Use Routes
 // app.use('/api/route', route);
+app.use('/api/auth', auth);
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
